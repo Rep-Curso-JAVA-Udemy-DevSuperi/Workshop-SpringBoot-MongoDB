62f30f9961c2b5d88e072aad34fe5198916b9234
Mateus Bonfanti <matebonfanti@gmail.com>
2024-03-19 17:31:53 -0300

    Inserção de Usuario com POST

--- src/main/java/com/mateusbonfanti/workshopmongo/services/UserService.java
+++ src/main/java/com/mateusbonfanti/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.mateusbonfanti.workshopmongo.domain.User;
+import com.mateusbonfanti.workshopmongo.dto.UserDTO;
 import com.mateusbonfanti.workshopmongo.repository.UserRepository;
 import com.mateusbonfanti.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,4 +25,15 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+		
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+			
+	}
 }
